b33a42fa7d2232a9c12f0d1dc3ee57a319646f36
RMarkov <rdnmarkov@gmail.com>
2023-10-18 09:13:04 +0300

    start project

---
+++ src/main/java/com/project/fxgames/FxgamesApplication.java
@@ -0,0 +1,13 @@
+package com.project.fxgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxgamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/fxgames/FxgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fxgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
